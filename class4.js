8dc0ff2b238c37589407dcd881fe6e98cce634db
Yisha Yan <yisha-y@hotmail.com>
2023-09-23 23:24:56 -0700

    first commit

---
+++ class4.js
@@ -0,0 +1,29 @@
+/**
+ * Incrementing
+ */
+var number = 0;
+console.log(number);
+
+number = number + 1;
+console.log(number);
+
+number += 1; // 1+1;
+console.log(number); // short have version;
+
+number++;
+console.log(number); // will only increase by one;
+
+/**
+ * Decrementing
+ */
+var value = 100;
+console.log(value);
+
+value = value - 1;
+console.log(value);
+
+value -= 1;
+console.log(value);
+
+value--;
+console.log(value); // will only decrease by one;
